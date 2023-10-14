06de55e53ef04e7742cab8a00a868f89fe8e3464
darylduck <daryl@lyraddigital.com>
2023-10-14 16:01:07 +1100

    Added code to validate the incoming session id in the call.

--- handlers/session.start-game-handler/src/index.js
+++ handlers/session.start-game-handler/src/index.js
@@ -1,4 +1,9 @@
-import { okResponseWithData, fatalErrorResponse } from "@oigamez/responses";
+import {
+  okResponseWithData,
+  fatalErrorResponse,
+  badRequestResponse,
+} from "@oigamez/responses";
+import { validateSessionId } from "@oigamez/validators";
 
 import { validateEnvironment } from "./configuration/index.js";
 
@@ -6,18 +11,29 @@ validateEnvironment();
 
 export const handler = async (event) => {
   try {
+    const sessionId = event.headers
+      ? event.headers["api-session-id"]
+      : undefined;
+
+    const validationResult = validateSessionId(sessionId);
+
+    if (!validationResult.isSuccessful) {
+      return badRequestResponse(validationResult.errorMessages);
+    }
+
     return okResponseWithData({});
   } catch (e) {
     console.log(e);
 
-    return fatalErrorResponse(
-      "Unknown issue while trying to connect to the game session socket server."
-    );
+    return fatalErrorResponse("Unknown issue while trying to start the game.");
   }
 };
 
 (async () => {
   const event = {
+    headers: {
+      ["api-session-id"]: "owfjwoifjwofjwoifjwfi",
+    },
     requestContext: {
       requestTimeEpoch: Date.now(),
     },
